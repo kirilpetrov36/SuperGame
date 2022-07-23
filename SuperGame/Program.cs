2f986389bec1c16342f1405b8e37b920eee1986e
Kirill <43477428+kirilpetrov36@users.noreply.github.com>
2022-07-23 17:44:53 +0300

    Random numbers generator

--- SuperGame/Program.cs
+++ SuperGame/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.WriteLine(SuperPowerGenerator.SuperPowerAmount());
+class SuperPowerGenerator
+{
+    public static int SuperPowerAmount()
+    {
+        Random rd = new Random();
+        int m = rd.Next(1, 1000);  //module
+        int x0 = rd.Next(0, m); ; //seed
+        int a = rd.Next(1, m);  // multiplier
+        int b = rd.Next(0, m);  // increment
+
+        x0 = (a * x0 + b) % m;  // randomness
+        double value = 5.0 + (5.0 * ((double)x0/1000.0));  // convert to value between 5 and 10
+        return (int)Math.Round(value, 0, MidpointRounding.ToEven);     
+    }
+}
+
+
